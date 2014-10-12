30700f3d907c3f0944c1f746767e3ca3754036db
michael <loptimus2510@gmail.com>
2014-10-12 18:33:54 +0800

    Auto Set Fonts

--- baseConfig/baseConfig.el
+++ baseConfig/baseConfig.el
@@ -2,24 +2,19 @@
 
 (provide 'baseConfig)
 
-
 ; Disable Startup display (关闭启动画面)
 ;(setq inhibit-startup-message t)
-
 
 ; (使用4个空格代替Tab)
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 4)
 ;(setq indent-line-function 'insert-tab)
 
-
 ; Disable backup file (不要生成临时文件)
 (setq-default make-backup-files nil)
 
 ; y/n instead of yes/no (用y/n代替yes/no)
 (fset 'yes-or-no-p 'y-or-n-p)
-
-
 
 ; Display date and time (显示时间)
 (display-time-mode 1)
@@ -37,7 +32,6 @@
 ;(require 'wb-line-number)
 ;(wb-line-number-toggle)
 
-
 ; Set (设置行距)
 ;(setq defaule-line-spaceing 4)
 
@@ -53,13 +47,57 @@
 ;; 闪屏警报
 ;(setq visible-bell t)
 
-
 ;; 取消提示音
 (setq ring-bell-function 'ignore)
 
 ;; 锁定行高
 ;(setq resize-mini-windows nil)
+ 
+;; 判断某个字体是否在系统中是否安装
+(defun qiang-font-existsp (font)
+ (if(null(x-list-fonts font))
+  nil t))
 
+;; 用来产生带上font size信息的font描述文本
+(defun qiang-make-font-string (font-name font-size)
+ (if(and(stringp font-size)
+     (equal":"(string (elt font-size 0))))
+  (format "%s%s" font-name font-size)
+  (format "%s %s" font-name font-size)))
+
+;; 自动设置字体函数
+(defun qiang-set-font (english-fonts
+                       english-font-size                       
+                       chinese-fonts                       
+                       &optional chinese-font-size)
+ "english-font-size could be set to \":pixelsize=18\" or a integer.If set/leave chinese-font-size to nil, it will follow english-font-size"
+ (require 'cl) ;; for find if
+ (let((en-font (qiang-make-font-string                  
+                (find-if #'qiang-font-existsp english-fonts)                  
+                english-font-size))
+      (zh-font (font-spec :family (find-if #'qiang-font-existsp chinese-fonts)
+                :size chinese-font-size)))     
+  ;; Set the default English font 
+  ;; The following 2 method cannot make the font settig work in new frames.
+  ;; (set-default-font "Consolas:pixelsize=18")
+  ;; (add-to-list 'default-frame-alist '(font . "Consolas:pixelsize=18"))
+  ;; We have to use set-face-attribute
+  (message "Set English Font to %s" en-font)
+  (set-face-attribute     
+   'default nil :font en-font)     
+  
+  ;; Set Chinese font
+  ;; Do not use 'unicode charset, it will cause the english font setting invalid
+  (message "Set Chinese Font to %s" zh-font)
+  (dolist(charset '(kana han symbol cjk-misc bopomofo))
+   (set-fontset-font (frame-parameter nil 'font)                        
+    charset                        
+    zh-font)))
+)
+
+;; 字体设置
+(qiang-set-font 
+ '("Consolas" "Monaco" "DejaVu Sans Mono" "Monospace" "Courier New") 14 '("Microsoft Yahei" "Kaiti SC" "文泉驿等宽微米黑" "黑体" "新宋体" "宋体"))
 
 ;; tabbar
 (require 'tabbar)
