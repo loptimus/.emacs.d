fbb1b47e0bd06087c6f34ee7ec563ca325e678be
michael <loptimus2510@gmail.com>
2014-10-13 01:03:56 +0800

    Add erlang tools/emacs and modified erlang-flymake

--- erlIDE/erlang_config.el
+++ erlIDE/erlang_config.el
@@ -86,27 +86,54 @@
 ;;===========================================FlymakeErlang语法检查=========================
 
 ;; 官方自带的，在erlang安装目录下/lib/tools<版本号>/emacs下
+(require 'erlang-flymake)
+
+
+;(defun get-erlang-app-dir ()
+;   (let* ((src-path (file-name-directory (buffer-file-name)))
+;               (pos (string-match "/src/" src-path)))
+;        (if pos
+;             (substring src-path 0 (+ 1 pos))
+;                   src-path)))
+;(setq erlang-flymake-get-code-path-dirs-function
+;       (lambda ()
+;            (concat (get-erlang-app-dir) "ebin")))
+;(defun erlang-flymake-self-get-include-dirs ()
+;  (list (concat (erlang-flymake-get-app-dir) "inc")
+;        (concat (erlang-flymake-get-app-dir) "../inc")
+;        (concat (erlang-flymake-get-app-dir) "../../inc")
+;  )
+;)
+;(setq erlang-flymake-get-include-dirs-function
+;      'erlang-flymake-self-get-include-dirs)
+
+;(setq erlang-flymake-get-code-include-dirs-function
+;      (lambda ()
+;        (concat (get-erlang-app-dir) "inc")
+;        (concat (get-erlang-app-dir) "../inc")
+;        (concat (get-erlang-app-dir) "../../inc")
+;        ))
 ;(require 'erlang-flymake)
 
 
 ;; http://www.emacswiki.org/emacs/FlymakeErlang
-(require 'flymake)
-(defun flymake-erlang-init ()
-  (let* ((temp-file (flymake-init-create-temp-buffer-copy
-		     'flymake-create-temp-inplace))
-	 (local-file (file-relative-name temp-file
-		(file-name-directory buffer-file-name))))
-    (list "~/.emacs.d/erlIDE/eflymake/eflymake.bat" (list local-file))))
+;(require 'flymake)
+;(defun flymake-erlang-init ()
+;  (let* ((temp-file (flymake-init-create-temp-buffer-copy
+;		     'flymake-create-temp-inplace))
+;	 (local-file (file-relative-name temp-file
+;		(file-name-directory buffer-file-name))))
+;    (list "~/.emacs.d/erlIDE/eflymake/eflymake_linux" (list local-file))))
 
-(add-to-list 'flymake-allowed-file-name-masks '("\\.erl\\'" flymake-erlang-init))
+;(add-to-list 'flymake-allowed-file-name-masks '("\\.erl\\'" flymake-erlang-init))
 
 ;;That's all, and you can either enable flymake globally, with the file open hook
 ;(add-hook 'find-file-hook 'flymake-find-file-hook)
 
 ;;or you can enable flymake mode only for some modes with corresponding hooks:
-(defun my-erlang-mode-hook ()
-       (flymake-mode 1))
-(add-hook 'erlang-mode-hook 'my-erlang-mode-hook)
+;(defun my-erlang-mode-hook ()
+;       (flymake-mode 1))
+;(add-hook 'erlang-mode-hook 'my-erlang-mode-hook)
 ;;=========================================FlymakeErlang=========================================
 
 
