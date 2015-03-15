ab41d6ad637cae2491654d65512bb31034a02476
liwenlong <liwenlong@syg.com>
2015-03-15 15:12:51 +0800

    [增加] cscope的环境变量，使在emacs中可以调用该工具

--- commonIDE/commonIDE_config.el
+++ commonIDE/commonIDE_config.el
@@ -103,6 +103,8 @@
 
 ;;; ======================================cscope=================================
 (defun cscope () "启用cscope"
+  ;(setenv "PATH" (concat (getenv "PATH") ":~/.emacs.d/commonIDE/cscope/bin"))
+  (setenv "PATH" (concat (getenv "PATH") ":" cscopePath "/bin"))
   (add-to-list 'load-path cscopePath)
   (require 'xcscope)
   (setq cscope-do-not-update-database t)
