273ec154c942abcb708cf56a602164387b720fb0
loptimus <loptimus2510@gmail.com>
2016-04-24 16:05:23 +0800

    [add] monokai-emacs

--- baseConf/base_conf.el
+++ baseConf/base_conf.el
@@ -175,18 +175,21 @@
 ;;; ==================== Color Theme =========================
 
 ; 启用font-lock-mode，在color-theme中会用到
-(global-font-lock-mode t)
+;(global-font-lock-mode t)
 
-(add-to-list 'load-path "~/.emacs.d/baseConf/color-theme-6.6.0/")
-(require 'color-theme)
-(color-theme-initialize)
+;(add-to-list 'load-path "~/.emacs.d/baseConf/color-theme-6.6.0/")
+;(require 'color-theme)
+;(color-theme-initialize)
 
 ;下列任选一种
 ;(color-theme-oswald)
 ;(color-theme-charcoal-black)
 ;(color-theme-deep-blue)
 ;(color-theme-ahei)
-(color-theme-molokai)
+;(color-theme-molokai)
+
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/monokai-emacs")
+(load-theme 'monokai t)
 
 ;; 保存时，删除行尾空格
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
